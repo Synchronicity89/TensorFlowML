7c16ff1adf770770ded16b47a53a8a21fab0df86
Synchronicity89 <15708443+Synchronicity89@users.noreply.github.com>
2023-11-13 15:13:32 -0800

    Using fourier feature mapping

--- TensorFTut.py
+++ TensorFTut.py
@@ -3,136 +3,142 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 
-class DiscontinuityLayer(tf.keras.layers.Layer):
-    def __init__(self, **kwargs):
-        super(DiscontinuityLayer, self).__init__(**kwargs)
+# Constants
+SIZE = 200000
+RANGE_X = (-10, 10)
+FOCUS_RANGE = (3.6, 4.4)
+FOCUS_SIZE = 100000
+NOISE_LEVEL = 0.1
+FREQUENCY_COUNT = 1024
+BATCH_SIZE = 1024 * 1
+EPOCHS = 10
+FOURIER_SCALE = 10
+DISCONTINUITY_INIT = 4.0  # Initial guess for the discontinuity point
+
+# Custom Modulation Layer
+class ModulationLayer(tf.keras.layers.Layer):
+    def __init__(self, enabled=True, **kwargs):
+        super(ModulationLayer, self).__init__(**kwargs)
+        self.enabled = enabled
     
     def build(self, input_shape):
-        self.discontinuity_point = self.add_weight(
-            name='discontinuity_point',
-            shape=(1,),
-            initializer=tf.keras.initializers.Constant(value=4.0),
+        self.modulation_param = self.add_weight(
+            name='modulation_param',
+            shape=(input_shape[-1],),
+            initializer='ones',
             trainable=True)
-        # Additional weight to control the smoothness of the transition
-        self.transition_width = self.add_weight(
-            name='transition_width',
-            shape=(1,),
-            initializer=tf.keras.initializers.Constant(value=1.0),
-            trainable=True)
-
+    
     def call(self, inputs):
-        inputs = tf.cast(inputs, tf.float32)
-        discontinuity_point = tf.cast(self.discontinuity_point, tf.float32)
-        transition_width = tf.cast(self.transition_width, tf.float32)
-        
-        # Use a sigmoid function to create a smooth transition
-        transition = tf.sigmoid((inputs - discontinuity_point) * transition_width)
-        below = tf.sin(5.0 * inputs) * (1 - transition)
-        above = tf.sin(8.0 * inputs) * transition
-        return below + above
+        if self.enabled:
+            return inputs * self.modulation_param
+        else:
+            return inputs
     
     def get_config(self):
         config = super().get_config()
-        config.update({'discontinuity_point': self.discontinuity_point.numpy(), 
-                       'transition_width': self.transition_width.numpy()})
+        config.update({'enabled': self.enabled})
         return config
 
-
+# Equations
 def eq_1(x):
-    return np.sin(5 * x) + np.sin(5 * x) + 0.1 * x ** 2
+    return 0.5 * np.tanh(x) + np.cos(x)
 
 def eq_2(x):
-    return np.sin(8 * x) + np.exp(0.2 * x) - 10
+    return np.log(np.abs(x) + 1) + 0.5 * x
+
+# Fourier Feature Mapping
+def fourier_feature_mapping(x, B):
+    x_fourier = np.concatenate([np.cos(x * B.T), np.sin(x * B.T)], axis=-1)
+    return x_fourier
 
 # Data Generation Function
-def generate_data(size=100000, range_x=(-10, 10), focus_range=(3.6, 4.4), focus_size=50000, noise=0.1):
+def generate_data(size, range_x, focus_range, focus_size, noise_level):
+    if size < focus_size:
+        raise ValueError("size must be greater than or equal to focus_size")
     x_focus = np.linspace(*focus_range, focus_size)
     x_sparse = np.linspace(*range_x, size - focus_size)
-    x_sparse = x_sparse[np.abs(x_sparse - 4) >= 1]
+    x_sparse = x_sparse[np.abs(x_sparse - DISCONTINUITY_INIT) >= 1]
     
-    # Reshape to ensure that x_focus and x_sparse are 2D arrays with a single column
-    # This is necessary for vertical concatenation using np.concatenate along axis=0
-    x = np.concatenate([x_focus.reshape(-1, 1), x_sparse.reshape(-1, 1)], axis=0)
-    y = np.where(x < 4, eq_1(x), eq_2(x))
-    y += noise * np.random.randn(*y.shape)
+    # Concatenate and reshape
+    x = np.concatenate([x_focus, x_sparse], axis=0).reshape(-1, 1)
+    y = np.where(x < DISCONTINUITY_INIT, eq_1(x), eq_2(x))
+    y += noise_level * np.random.randn(*y.shape)
     return x, y
 
-def train():
-    x_train, y_train = generate_data()
+# Generating random Fourier features
+B = np.random.normal(size=(FREQUENCY_COUNT,)) * FOURIER_SCALE
+x_train, y_train = generate_data(SIZE, RANGE_X, FOCUS_RANGE, FOCUS_SIZE, NOISE_LEVEL)
+x_train_fourier = fourier_feature_mapping(x_train, B)
 
-    train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-    train_dataset = train_dataset.cache().shuffle(buffer_size=10000).batch(1024*4).prefetch(tf.data.AUTOTUNE)
+# Training Function
+def train(use_modulation_layer):
+    train_dataset = tf.data.Dataset.from_tensor_slices((x_train_fourier, y_train))
+    train_dataset = train_dataset.cache().shuffle(buffer_size=SIZE).batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
 
-    model = tf.keras.Sequential([
-        DiscontinuityLayer(input_shape=(1,)),
-        tf.keras.layers.Dense(20, activation='relu'),
-        tf.keras.layers.Dense(40, activation='relu'),
-        tf.keras.layers.Dense(60, activation='relu'),
-        tf.keras.layers.Dense(40, activation='relu'),
-        tf.keras.layers.Dense(20, activation='relu'),
-        tf.keras.layers.Dense(1)
-    ])
+    model = tf.keras.Sequential()
+    if use_modulation_layer:
+        model.add(ModulationLayer(input_shape=(FREQUENCY_COUNT * 2,)))  # Adjust input shape for the ModulationLayer
+    
+    model.add(tf.keras.layers.Dense(20, activation='relu', input_shape=(FREQUENCY_COUNT * 2,)))
+    model.add(tf.keras.layers.Dense(40, activation='relu'))
+    model.add(tf.keras.layers.Dense(60, activation='relu'))
+    model.add(tf.keras.layers.Dense(40, activation='relu'))
+    model.add(tf.keras.layers.Dense(20, activation='relu'))
+    model.add(tf.keras.layers.Dense(1))
 
     model.compile(optimizer='adam', loss='mean_squared_error')
 
     start_time = time.time()
-    history = model.fit(train_dataset, epochs=400, verbose=1)
+    history = model.fit(train_dataset, epochs=EPOCHS, verbose=1)
     training_time = time.time() - start_time
     print(f"Training time: {training_time} seconds")
 
     model.save('my_model.h5')
+    return model, history
 
-    # Plot training loss
-    plt.plot(history.history['loss'])
-    plt.title('Model loss')
-    plt.ylabel('Loss')
-    plt.xlabel('Epoch')
-    plt.show()
-
-    return model
-
+# Main execution
 if __name__ == '__main__':
-    trained_model = train()  # Uncomment to train the model
-
-    # Plot the Discontinuity Layer Activation
-    discontinuity_layer = trained_model.layers[0]
-    discontinuity_point_weight = discontinuity_layer.get_weights()[0]
-    print("Trained Discontinuity Point Weight:", discontinuity_point_weight)
+    # Train with the ModulationLayer enabled
+    model_with_modulation, history_with_modulation = train(use_modulation_layer=True)
     
-    # Generate test data from -15 to 15
+    # Train without the ModulationLayer
+    model_without_modulation, history_without_modulation = train(use_modulation_layer=False)
+    
+    # Generate test data and apply Fourier mapping
     x_test = np.linspace(-15, 15, 300).reshape(-1, 1)
-    y_actual = np.where(x_test < 4, eq_1(x_test), eq_2(x_test))
-    y_predicted = trained_model.predict(x_test)
+    x_test_fourier = fourier_feature_mapping(x_test, B)
+    y_actual = np.where(x_test < DISCONTINUITY_INIT, eq_1(x_test), eq_2(x_test))
+    
+    # Predict with both models
+    y_predicted_with_modulation = model_with_modulation.predict(x_test_fourier)
+    y_predicted_without_modulation = model_without_modulation.predict(x_test_fourier)
 
-    # Plotting
+    # Plotting for model with modulation
     plt.figure(figsize=(10, 6))
     plt.scatter(x_test, y_actual, color='blue', label='Actual')
-    plt.scatter(x_test, y_predicted, color='red', alpha=0.7, label='Predicted')
-    plt.title('Comparison of Actual and Predicted Values')
+    plt.scatter(x_test, y_predicted_with_modulation, color='red', alpha=0.7, label='Predicted with Modulation')
+    plt.title('Comparison with Modulation Layer')
     plt.xlabel('x')
     plt.ylabel('y')
     plt.legend()
     plt.show()
 
-
-    # After training
-    discontinuity_layer = trained_model.layers[0]  # Assuming the DiscontinuityLayer is the first layer
-    discontinuity_point = discontinuity_layer.get_weights()[0]  # Retrieve the discontinuity point weight
-
-    # Report the discontinuity point weight
-    print("Discontinuity Point Weight:")
-    print(discontinuity_point)
-
-    # Visualize the Discontinuity Layer Activation
-    test_inputs = np.linspace(-15, 15, 300).reshape(-1, 1)
-    discontinuity_activation = discontinuity_layer(test_inputs)
-
-    # Plot the activation pattern
+    # Plotting for model without modulation
     plt.figure(figsize=(10, 6))
-    plt.plot(test_inputs, discontinuity_activation)
-    plt.title('Discontinuity Layer Activation Pattern')
-    plt.xlabel('Input Value')
-    plt.ylabel('Layer Activation')
-    plt.axvline(x=discontinuity_point, color='grey', linestyle='--', label=f'Discontinuity Point ~ {discontinuity_point[0]:.2f}')
+    plt.scatter(x_test, y_actual, color='blue', label='Actual')
+    plt.scatter(x_test, y_predicted_without_modulation, color='green', alpha=0.7, label='Predicted without Modulation')
+    plt.title('Comparison without Modulation Layer')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend()
+    plt.show()
+    
+    # Plot training loss for both models
+    plt.figure(figsize=(10, 6))
+    plt.plot(history_with_modulation.history['loss'], label='With Modulation')
+    plt.plot(history_without_modulation.history['loss'], label='Without Modulation')
+    plt.title('Training Loss Comparison')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
     plt.legend()
     plt.show()
