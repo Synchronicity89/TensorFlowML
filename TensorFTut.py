b0c72e90933cdf68a85af5fa0573e6ad7f566d10
Synchronicity89 <15708443+Synchronicity89@users.noreply.github.com>
2023-11-12 19:38:00 -0800

    Good version quick train, accurate predictions

--- TensorFTut.py
+++ TensorFTut.py
@@ -2,54 +2,79 @@ import tensorflow as tf
 import numpy as np
 import time
 
-print("TensorFlow Version:", tf.__version__)
-gpus = tf.config.list_physical_devices('GPU')
-print("Num GPUs Available: ", len(gpus))
-for gpu in gpus:
-    print("Name:", gpu.name, " Type:", gpu.device_type)
+def eq_1(x):
+    return 0.5 * x ** 2 + 5 * x + 11
 
+def eq_2(x):
+    return 0.4 * x ** 2 + 9 * x + 7
 
 # Data Generation Function
 def generate_data(size=100000, range_x=(-10, 10), focus_range=(3, 5), focus_size=50000, noise=0.1):
-    x_focus = np.linspace(*focus_range, focus_size)
-    x_sparse = np.linspace(*range_x, size - focus_size)
+    x_focus = np.linspace(*focus_range, focus_size).reshape(-1, 1)
+    x_sparse = np.linspace(*range_x, size - focus_size).reshape(-1, 1)
     x_sparse = x_sparse[np.abs(x_sparse - 4) >= 1]
     x = np.concatenate([x_focus.reshape(-1, 1), x_sparse.reshape(-1, 1)], axis=0)
-    y = np.where(x < 4, 0.5 * x ** 2 + 6 * x + 8, 0.4 * x ** 2 + 7 * x + 9)
+    y = np.where(x < 4, eq_1(x), eq_2(x))
     y += noise * np.random.randn(*y.shape)
     return x, y
 
-# Generate Data
-x_train, y_train = generate_data()
+def train():
+    # Generate Data
+    x_train, y_train = generate_data()
 
-# Model Definition
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(20, activation='relu', input_shape=(1,)),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(40, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(20, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(1)
-])
+    # Convert to TensorFlow dataset and optimize for performance
+    train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    train_dataset = train_dataset.cache().shuffle(buffer_size=100000).batch(2048).prefetch(tf.data.AUTOTUNE)
 
-# Compile the Model
-model.compile(optimizer='adam', loss='mean_squared_error')
+    # Model Definition
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(20, activation='relu', input_shape=(1,)),
+        tf.keras.layers.Dense(40, activation='relu'),
+        tf.keras.layers.Dense(20, activation='relu'),
+        tf.keras.layers.Dense(1)
+    ])
 
-# Train the Model
-start_time = time.time()
-model.fit(x_train, y_train, epochs=200, batch_size=256, verbose=1)
-training_time = time.time() - start_time
-print(f"Training time: {training_time} seconds")
+    # Compile the Model
+    model.compile(optimizer='adam', loss='mean_squared_error')
 
-# Save the Model
-model.save('my_model.h5')
+    # Train the Model
+    start_time = time.time()
+    model.fit(train_dataset, epochs=200, verbose=1)
+    training_time = time.time() - start_time
+    print(f"Training time: {training_time} seconds")
 
-# Load and Test the Model
-new_model = tf.keras.models.load_model('my_model.h5')
-test_inputs = np.array([[-5], [0], [5]])
-model_outputs = new_model.predict(test_inputs)
-expected_outputs = np.where(test_inputs < 4, 0.5 * test_inputs ** 2 + 6 * test_inputs + 8, 0.4 * test_inputs ** 2 + 7 * test_inputs + 9)
+    # Save and Load the Model for Testing
+    model.save('my_model.h5')
+    new_model = tf.keras.models.load_model('my_model.h5')
 
-for inp, out, exp in zip(test_inputs, model_outputs, expected_outputs):
-    print(f"Input: {inp[0]}, Model Output: {out[0]}, Expected Output: {exp[0]}")
+    # Testing
+    test_inputs = np.array([[-5], [0], [5]])
+    model_outputs = new_model.predict(test_inputs)
+    expected_outputs = np.where(test_inputs < 4, eq_1(test_inputs), eq_2(test_inputs))
+
+    for inp, out, exp in zip(test_inputs, model_outputs, expected_outputs):
+        print(f"Input: {inp[0]}, Model Output: {out[0]}, Expected Output: {exp[0]}")
+if __name__ == '__main__':
+    train()  # Uncomment this line to train the model
+
+    new_model = tf.keras.models.load_model('my_model.h5')
+                                           
+    import matplotlib.pyplot as plt
+
+    # Generate test data from -15 to 15
+    x_test = np.linspace(-15, 15, 300).reshape(-1, 1)
+    y_actual = np.where(x_test < 4, eq_1(x_test), eq_2(x_test))
+
+    # Predict using the model
+    y_predicted = new_model.predict(x_test)
+
+    # Plotting
+    plt.figure(figsize=(10, 6))
+    plt.scatter(x_test, y_actual, color='blue', label='Actual')
+    plt.scatter(x_test, y_predicted, color='red', alpha=0.7, label='Predicted')
+    plt.title('Comparison of Actual and Predicted Values')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend()
+    plt.show()
+                                     
